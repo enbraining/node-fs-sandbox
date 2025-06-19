9f334be7f3812bb28dccb2d5b45eb5ca0f23ac0c
enbraining <me@bricn.net>
2025-06-19 17:29:16 +0900

    리스트 불러오기 구현

--- app/help/route.ts
+++ app/help/route.ts
@@ -1,9 +1,18 @@
 import fs from "fs";
 import { NextResponse } from "next/server";
+import path from "path";
 
 export async function GET() {
-  const filePath = `${process.cwd()}/public/help.md`;
-  const data = fs.readFileSync(filePath, "utf8");
+  const directoryPath = path.join(process.cwd(), "public", "contents");
 
-  return new NextResponse(data);
+  const files = fs.readdirSync(directoryPath);
+  const fileContents = files.map((value) => {
+    const filePath = path.join(process.cwd(), "public", "contents", value);
+
+    return {
+      content: fs.readFileSync(filePath, "utf8"),
+    };
+  });
+
+  return NextResponse.json(fileContents);
 }
